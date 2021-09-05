b90a8aabf9db5facfec1439262bfc5f03083c42d
dolphin <dolphin@163.com>
2021-09-05 21:19:38 +0800

    add:添加长轮询demo

--- README.md
+++ README.md
@@ -20,5 +20,5 @@ SpringBoot总结教程
 - [全局异常处理](https://github.com/ericql/spring-boot-tutorial/tree/master/spring-boot-web/spring-boot-global-exception/global-exception.md)
 - [静态资源处理](https://github.com/ericql/spring-boot-tutorial/tree/master/spring-boot-web/static-resource-handler.md)
 
-## SpringBoot优雅停机
+## SpringBoot之优雅停机
 [优雅停机教程](https://github.com/ericql/spring-boot-tutorial/blob/master/spring-boot-graceful-shutdown/graceful-shutdown.md)
---
+++ spring-boot-longpolling/src/main/java/com/eric/longpolling/ConfigServer.java
@@ -0,0 +1,96 @@
+package com.eric.longpolling;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author dolphin
+ * @date 2021-01-23
+ */
+@RestController
+@Slf4j
+@SpringBootApplication
+public class ConfigServer {
+
+    @Data
+    private static class AsyncTask {
+        // 长轮询请求的上下文，包含请求和响应体
+        private AsyncContext asyncContext;
+        // 超时标记
+        private boolean timeout;
+
+        public AsyncTask(AsyncContext asyncContext, boolean timeout) {
+            this.asyncContext = asyncContext;
+            this.timeout = timeout;
+        }
+    }
+
+    // guava 提供的多值 Map，一个 key 可以对应多个 value
+    private volatile Multimap<String, AsyncTask> dataIdContext = Multimaps.synchronizedSetMultimap(HashMultimap
+        .create());
+
+    private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("longPolling-timeout-checker-%d")
+        .build();
+    private ScheduledExecutorService timeoutChecker = new ScheduledThreadPoolExecutor(1, threadFactory);
+
+    // ① 监听接入点
+    @RequestMapping("/listener")
+    public void addListener(HttpServletRequest request, HttpServletResponse response) {
+
+        String dataId = request.getParameter("dataId");
+
+        // ② 开启异步
+        AsyncContext asyncContext = request.startAsync(request, response);
+        AsyncTask asyncTask = new AsyncTask(asyncContext, true);
+
+        dataIdContext.put(dataId, asyncTask);
+        // ③ 启动定时器，30s 后写入 304 响应
+        timeoutChecker.schedule(() -> {
+            if (asyncTask.isTimeout()) {
+                dataIdContext.remove(dataId, asyncTask);
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                asyncContext.complete();
+            }
+        }, 30000, TimeUnit.MILLISECONDS);
+    }
+
+    // ④ 配置发布接入点
+    @RequestMapping("/publishConfig")
+    @SneakyThrows
+    public String publishConfig(String dataId, String configInfo) {
+        log.info("publish configInfo dataId: [{}], configInfo: {}", dataId, configInfo);
+        Collection<AsyncTask> asyncTasks = dataIdContext.removeAll(dataId);
+
+        for (AsyncTask asyncTask : asyncTasks) {
+            asyncTask.setTimeout(false);
+            HttpServletResponse response = (HttpServletResponse)asyncTask.getAsyncContext().getResponse();
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(configInfo);
+            asyncTask.getAsyncContext().complete();
+        }
+        return "success";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
+}
